ffef2051e07e2ad12e1b2e874fcf19d33ece2b66
t-nan <t-nan@yandex.ru>
2020-11-29 16:03:19 +0300

    add navigation on pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,10 +8,11 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article=Article.find(params[:id])
+   @article=Article.find(params[:id])    
   end
 
   def new
+    @article=Article.new
   end
 
   def create
